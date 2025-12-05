5cc2f867426d39684c1b951f1bd10df428cb0c75
obiinnaa-codes <bookobiinnaa@gmail.com>
2025-12-05 11:25:57 +0100

    feat: Implement Sentry error tracking

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:unblur_images/features/auth/presentation/login_screen.dart';
 import 'package:unblur_images/features/home/presentation/home_screen.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,7 +14,22 @@ void main() async {
     anonKey: SupabaseConstants.anonKey,
   );
 
-  runApp(const ProviderScope(child: MyApp()));
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://1fc9fcfa26f584b6d2eba513ffdfd723@o4510480770990080.ingest.us.sentry.io/4510480772694016';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.profilesSampleRate = 1.0;
+    },
+    appRunner: () =>
+        runApp(SentryWidget(child: const ProviderScope(child: MyApp()))),
+  );
+  // TODO: Remove this line after sending the first sample event to sentry.
+  await Sentry.captureException(Exception('This is a sample exception.'));
 }
 
 class MyApp extends StatelessWidget {
@@ -23,6 +39,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AI Image Enhancer',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
